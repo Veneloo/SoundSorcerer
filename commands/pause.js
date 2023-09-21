b6b5c588ed09f52cfdae489177841af1f647f25b
Veneloo <axel.cazorla10@gmail.com>
2023-09-21 17:09:10 -0400

    Added Implemenation for Pause.js

---
+++ commands/pause.js
@@ -0,0 +1,29 @@
+/*
+Axel Cazorla
+9/21/2023
+
+This file will be for the action of "Pause" in our music bot
+*/
+
+const {SlashCommandBuilder} = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()     //We create the slash command to pause ("/pause")
+        .setName("pause")
+        .setDescription("Pauses the current song."),
+    execute: async ({client, interaction}) => {
+
+        const queue = client.player.getQueue(interaction.guild);
+
+        if (!queue) {                                               //First we check if there is a song playing
+            await interaction.reply("There is no song playing.");   //if there iusnt we let the user know
+            return;
+        }
+
+
+        queue.setPaused(true);                                           //if there is song playing we simply call the Pause Function
+
+        await interaction.reply("The current song has been paused.");   //when the command is executed we let the user know which song was paused
+    }
+}
--- commands/skip.js
+++ commands/skip.js
@@ -4,3 +4,33 @@ Axel Cazorla
 
 This file will be for the action of "Skip" in our music bot
 */
+
+const {SlashCommandBuilder} = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()     //We create the slash command to skip ("/skip")
+        .setName("skip")
+        .setDescription("Skips the current song."),
+    execute: async ({client, interaction}) => {
+
+        const queue = client.player.getQueue(interaction.guild);
+
+        if (!queue) {                                               //First we check if there is a song playing
+            await interaction.reply("There is no song playing.");   //if there iusnt we let the user know
+            return;
+        }
+
+        const currentSong = queue.current;
+
+        queue.skip();                                           //if there is song playing we simply call the skip function
+
+        await interaction.reply({
+            embeds: [
+                new MessageEmbed()
+                    .setDescription('Skipped **${currentSong.title}**')     //When the command executes it will let us know the song we skipped
+                    .setThumbnail(currentSong.thumbnail)
+            ]
+        })
+    }
+}
