5c9bf75edebd2eba642c9f9013a25c5a552af8e5
Veneloo <axel.cazorla10@gmail.com>
2023-09-21 16:55:54 -0400

    Added Commentary in Play.js

--- commands/play.js
+++ commands/play.js
@@ -45,10 +45,10 @@ module.exports = {
                         .setRequired(true);
                 })
         }),
-        execute: async ({client, interaction}) => {
+        execute: async ({client, interaction}) => {         //This will actually execute our commands
             if (!interaction.member.voice.channel)
             {
-                await interaction.reply("You must be in a voice channel to use this command")
+                await interaction.reply("You must be in a voice channel to use this command")   //First we check if the user is in a voice channel if not, we will prompt a message that he needs to join
                 return;
             }
 
@@ -57,41 +57,41 @@ module.exports = {
             if(!queue.connection) await queue.connect(interaction.member.voice.channel)
 
             let embed = new MessageEmbed();
-            if(interaction.options.getSubcommand() === "song")
+            if(interaction.options.getSubcommand() === "song")          //if the user picked the song command it will wait for the url of the desired song
             {
                 let url = interaction.options.getString("url");
 
                 const result = await client.player.search(url, {
                     requestedBy: interaction.user,
-                    searchEngine: QueryType.YOUTUBE_VIDEO,
+                    searchEngine: QueryType.YOUTUBE_VIDEO,          //it will expect a youtube video url
                 });
 
                 if(result.tracks.length === 0)
                 {
-                    await interaction.reply("no results found/invalid choice")
+                    await interaction.reply("no results found/invalid choice")      //in case the duration of the song is 0 or is not available it will send a message letting the user know about the error
                     return
                 }
 
-                const song = result.tracks[0]
+                const song = result.tracks[0]                           
                 await queue.addTrack(song);
 
                 embed
-                    .setDescription('Added **[${song.title}](${song.url})** to queue')
+                    .setDescription('Added **[${song.title}](${song.url})** to queue')  //This will add the songs to the queue
                     .setThumbnail(song.thumbnail)
                     .setFooter({text: 'Duration: ${song.duration}'});
             }
-            else if(interaction.options.getSubcommand() === "playlist")
+            else if(interaction.options.getSubcommand() === "playlist")     //If the user picked the playlist command it will wait for the playlist url
             {
                 let url = interaction.options.getString("url");
 
                 const result = await client.player.search(url, {
                     requestedBy: interaction.user,
-                    searchEngine: QueryType.YOUTUBE_PLAYLIST,
+                    searchEngine: QueryType.YOUTUBE_PLAYLIST,           //It expects to recieve a youtube playlist link
                 });
 
                 if(result.tracks.length === 0)
                 {
-                    await interaction.reply("no playlist found/invalid choice")
+                    await interaction.reply("no playlist found/invalid choice")     //If the playlist has no content or is not available it will let the user know
                     return
                 }
 
@@ -99,22 +99,22 @@ module.exports = {
                 await queue.addTracks(song);
 
                 embed
-                    .setDescription('Added **[${playlist.title}](${playlist.url})** to queue')
+                    .setDescription('Added **[${playlist.title}](${playlist.url})** to queue')  //This simply adds the playlist to the queue
                     .setThumbnail(playlist.thumbnail)
                     .setFooter({text: 'Duration: ${playlist.duration}'});
             }
-            else if(interaction.options.getSubcommand() === "search")
+            else if(interaction.options.getSubcommand() === "search")       //in case the user chose the search command it will expect a keyword
             {
                 let url = interaction.options.getString("searchterms");
 
                 const result = await client.player.search(url, {
                     requestedBy: interaction.user,
-                    searchEngine: QueryType.AUTO,
+                    searchEngine: QueryType.AUTO,               //The auto will look for any keyword matching the word from the input
                 });
 
                 if(result.tracks.length === 0)
                 {
-                    await interaction.reply("no results found/invalid choice")
+                    await interaction.reply("no results found/invalid choice")  //if the search was invalid or not available it will let the user know
                     return
                 }
 
@@ -122,14 +122,14 @@ module.exports = {
                 await queue.addTracks(song);
 
                 embed
-                    .setDescription('Added **[${song.title}](${song.url})** to queue')
+                    .setDescription('Added **[${song.title}](${song.url})** to queue')  //This simply adds the song to the queue
                     .setThumbnail(song.thumbnail)
                     .setFooter({text: 'Duration: ${song.duration}'});
             }
+            //here we actually start playing the music
+            if(!queue.playing) await queue.play();  //We first check that if there isnt music already playing we start playing music
 
-            if(!queue.playing) await queue.play();
-
-            await interaction.reply({
+            await interaction.reply({       //This simply just takes the embeded entries from the interactions to start playing 
                 embeds: [embed]
             })
 
